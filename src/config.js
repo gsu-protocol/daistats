8f11a1c5250f138575aa9bda78f4f56f35ef3945
iamsalmandev <dev.iamsalman@gmail.com>
2022-10-11 15:03:35 +0500

    updates config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const configs={
     ETHERSCAN_URL:'https://etherscan.io',
-    CHAINLOG_URL:'https://etherscan.io',
+    CHAINLOG_URL:'chainlog-dev.gsuprotocol.io',
     ETHERSCAN_ETHSUPPLY_URL:'https://api.etherscan.io/api?action=ethsupply&module=stats&apikey=N5TICDBVG4MHDS7CGPJ9MHXRYC1Y84963N'
 }
