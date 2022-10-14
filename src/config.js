3a0cceec6d63442ed0999eb4be08cb05f09f8bc7
iamsalmandev <dev.iamsalman@gmail.com>
2022-10-14 16:00:18 +0500

    removes unneeded addresses

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export const configs={
+    NETWORK_ID:5,
+    INFURA_APP_ID:"4278656ceb864ffbac367329fefb5dfe",
     ETHERSCAN_URL:'https://etherscan.io',
     CHAINLOG_URL:'https://chainlog-dev.gsuprotocol.io',
     ETHERSCAN_ETHSUPPLY_URL:'https://api.etherscan.io/api?action=ethsupply&module=stats&apikey=N5TICDBVG4MHDS7CGPJ9MHXRYC1Y84963N'
--- src/web3.jsx
+++ src/web3.jsx
@@ -1,3 +1,5 @@
+import { configs } from './config'
+const { NETWORK_ID, INFURA_APP_ID } = configs
 const ethers = require('ethers')
 window.ethers = ethers
 const eth = new ethers.providers.InfuraProvider()
